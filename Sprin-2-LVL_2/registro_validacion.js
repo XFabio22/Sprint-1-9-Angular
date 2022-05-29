e773895eb37743b8099c13b47de661fba3351939
Fabio Aguilar <fabioaguilar222@gmail.com>
2022-05-29 20:36:17 +0200

    Sprint 2

---
+++ Sprin-2-LVL_2/registro_validacion.js
@@ -0,0 +1,51 @@
+$(function() {
+
+    $.validator.setDefaults({
+      errorClass: 'invalid-feedback',
+      highlight: function(element) {
+        $(element)
+  
+          .addClass('is-invalid');
+      },
+      unhighlight: function(element) {
+        $(element)
+  
+          .removeClass('is-invalid');
+      },
+      errorPlacement: function (error, element) {
+        if (element.prop('type') === 'checkbox') {
+          error.insertAfter(element.parent());
+        } else {
+          error.insertAfter(element);
+        }
+      }
+    });
+  
+   
+    $("#register-form").validate({
+      rules: {
+        registrationemail: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        "registrationemail": {
+              required: "Este campo es obligatorio.",
+              email: "Por favor, escribe una dirección de correo válida"
+          },
+            
+      }
+    });
+  
+      $("#register-form").on('submit', function() {
+          if ($('#register-form').valid()) {
+              alert("¡Correo valido");
+          } else {
+              alert("Correo no valio");
+          }
+      });
+  
+  });
+  
+  
